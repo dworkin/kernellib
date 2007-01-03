f18613a3dfbdb4b7f109da815d8cce07f87d5765
elemel <elemel@35ddd1ce-1426-0410-96b0-9c6f07de3b7d>
2007-01-03 22:09:26 +0000

    Reserved TLS space for create() arguments. Commented code. Now initializes user code in call-outs.
    git-svn-id: https://leprechaun.svn.sourceforge.net/svnroot/leprechaun/trunk@5 35ddd1ce-1426-0410-96b0-9c6f07de3b7d

--- mud/usr/System/initd.c
+++ mud/usr/System/initd.c
@@ -1,26 +1,47 @@
-#include <kernel/kernel.h>
-#include <kernel/rsrc.h>
-#include <system/object.h>
-#include <system/system.h>
+# include <config.h>
+# include <kernel/kernel.h>
+# include <kernel/rsrc.h>
+# include <kernel/tls.h>
+# include <system/object.h>
+# include <system/system.h>
 
-private inherit api_rsrc API_RSRC;
+private inherit api_rsrc  API_RSRC;
+private inherit api_tls   API_TLS;
 
-private object load(string path) {
+/*
+ * NAME:        load
+ * DESCRIPTION: find or compile an object
+ */
+private object load(string path)
+{
     object obj;
 
     obj = find_object(path);
     if (!obj) {
         obj = compile_object(path);
     }
-    call_other(obj, "???");
+    if (obj) {
+	call_other(obj, "???");
+    }
     return obj;
 }
 
-static void create() {
-    int i, size;
-    string *owners, path;
+/*
+ * NAME:        create
+ * DESCRIPTION: initialize system initd
+ */
+static void create()
+{
+    int      i, size;
+    string  *owners, path;
 
     api_rsrc::create();
+    api_tls::create();
+
+    DRIVER->message("Initializing system.\n");
+
+    /* reserve TLS space for create() arguments */
+    api_tls::set_tls_size(1);
 
     /* install object manager */
     DRIVER->set_object_manager(load(OBJECTD));
@@ -32,15 +53,23 @@ static void create() {
     owners = api_rsrc::query_owners() - ({ nil, "System" });
     size = sizeof(owners);
     for (i = 0; i < size; ++i) {
-        path = "/usr/" + owners[i] + "/initd";
+        path = USR_DIR + "/" + owners[i] + "/initd";
         if (DRIVER->file_size(path + ".c")) {
-            catch {
-                call_limited("init", path);
-            }
+	    /*
+	     * the increased TLS size does not affect the current execution
+	     * round, so initialize user code in call-outs
+	     */
+	    call_out("init", path);
         }
     }
 }
 
-static void init(string path) {
+/*
+ * NAME:        init
+ * DESCRIPTION: create user initd after call-out
+ */
+static void init(string path)
+{
+    DRIVER->message("Initializing " + path + ".\n");
     load(path);
 }
